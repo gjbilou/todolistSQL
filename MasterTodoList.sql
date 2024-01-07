407659cfba90d83336722546870005b46befbde7
boulhanna <ilyass.boulhanna.etu.unistra.fr>
2024-01-07 22:35:21 +0100

    rapport and master

--- MasterTodoList.sql
+++ MasterTodoList.sql
@@ -4,6 +4,13 @@
  --executer le fichier de creation de tables 
  @./TodoListCreationTable.sql
  
+ --executer le fichier triggers
+ @./TodoListTriggers.sql
+ 
  --executer le fichier d'insertion de données dans la BDD
  @./TodoListInsertion.sql
+ 
+ --executer le fichier des procedures
+ @./TodoListProcedures.sql
+
  
