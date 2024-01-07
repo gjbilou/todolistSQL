3ca9684f2b54923a3d028fe440df669d1fb71454
JBILOU GHAIT <gha.jbilou@gmail.com>
2024-01-07 20:32:17 +0100

    ajout indentation

--- TodoListProcedures.sql
+++ TodoListProcedures.sql
@@ -1,13 +1,10 @@
 
 set serveroutput on;
 
---version basique de la premiere fonction
 create or replace procedure nbPoints 
 is
 
--- we need a cursor to loop through the select of programs and tasks
 	cursor c1 is
-		-- i need to select all users with a custom program
 		select * 
 		from programmescore 
 		join taches 
@@ -22,20 +19,16 @@ is
 
 begin
 	for c1_rec in c1 loop
-		-- we need to check if the task was finished on time or not on time (we won't check unterminated tasks : before the deadline)
 		if c1_rec.dateEcheance > SYSDATE and c1_rec.status = 1 
 		then 
-			-- if the task was finished on time, we need to add the points to the totalPositif
 			totalPositif := totalPositif + c1_rec.scoreToAdd;
 
 		elsif c1_rec.dateEcheance < SYSDATE and c1_rec.status = 1 
 		then 
-			-- if the task was finished, but we checked it late, then we need to add the points to the totalPositif
 			totalPositif := totalPositif + c1_rec.scoreToAdd;
 
 		elsif c1_rec.dateEcheance < SYSDATE and c1_rec.status = 0
 		then 
-			-- if the task was not finished on time, we need to add the points to the totalNegatif
 			totalNegatif := totalNegatif - c1_rec.scoreToSub;
 
 		end if;
@@ -53,7 +46,6 @@ end;
 
 
 
--- i need a procedure that will verify each week if the tasks are done or not (and if they are done, move them to taskfinished and add score to user)
 create or replace procedure archiveTasks
 is
 	pragma autonomous_transaction;
@@ -63,22 +55,16 @@ is
 		where dateEcheance < SYSDATE 
 		or status = 1;
 begin
-	-- i would like to loop through the cursor and move the tasks to taskfinished 
 	for c1_rec in c1 loop
-		-- i need to check if the task is done or not
-		-- if the task is done, i need to move it to taskfinished
 		insert into tachefini values (c1_rec.idTache, c1_rec.intitule, c1_rec.dateEcheance, c1_rec.lienExterne, c1_rec.categorie, c1_rec.status, c1_rec.idCreateur, c1_rec.idListe);
 
-		-- i need to delete the task from tachesencours
 		delete from tachesencours
 		where idTache = c1_rec.idTache;
 	end loop;
 
--- i just need to check the score (when will it be updated ?)
 	commit;
 end archiveTasks;
 
--- i need to automate this procedure to run every week
 declare
 	processId number;
 	
--- TodoListTriggers.sql
+++ TodoListTriggers.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TRIGGER UpdateUserScore
+CREATE OR REPLACE TRIGGER updateScoreArchive
 AFTER INSERT ON TACHEFINI
 FOR EACH ROW
 DECLARE
@@ -9,16 +9,12 @@ DECLARE
 
 BEGIN
 	IF :NEW.status = 0 THEN
-		-- Pour chaque utilisateur associé à la tâche
 		FOR userRecord IN c1 LOOP
-			-- Vérifier si l'utilisateur a un programme score
 			SELECT COUNT(*) INTO userHasProgrammeScore FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
 
 			IF userHasProgrammeScore > 0 THEN
-				-- Récupérer le score à soustraire
 				SELECT scoreToSub INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
 			ELSE
-				-- Utiliser la valeur par défaut
 				scoreChange := 5;
 			END IF;
 
@@ -29,114 +25,96 @@ BEGIN
 END;
 /
 
--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-CREATE OR REPLACE TRIGGER UpdateScoreAfterTaskCompletion
+CREATE OR REPLACE TRIGGER updateScoreCompletion
 AFTER UPDATE OF status ON TACHENCOURS
 FOR EACH ROW
 WHEN (NEW.status = 1)
 DECLARE
-    scoreChange NUMBER;
-    userHasProgrammeScore NUMBER;
-    cursor c1 is
-        SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache;
+	scoreChange NUMBER;
+	userHasProgrammeScore NUMBER;
+	cursor c1 is
+		SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache;
 BEGIN
-    -- Mise à jour de la date d'accomplissement de la tâche
-    UPDATE TACHENCOURS SET dateAccomplissement = SYSDATE WHERE idTache = :NEW.idTache;
+	UPDATE TACHENCOURS SET dateAccomplissement = SYSDATE WHERE idTache = :NEW.idTache;
 
-    -- Pour chaque utilisateur associé à la tâche
-    FOR userRecord IN c1 LOOP
-        -- Vérifier si l'utilisateur a un programme score
-        SELECT COUNT(*) INTO userHasProgrammeScore FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
+	FOR userRecord IN c1 LOOP
+		SELECT COUNT(*) INTO userHasProgrammeScore FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
 
-        IF userHasProgrammeScore > 0 THEN
-            -- Récupérer le score à ajouter
-            SELECT scoreToAdd INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
-        ELSE
-            -- Utiliser la valeur par défaut
-            scoreChange := 5;
-        END IF;
+		IF userHasProgrammeScore > 0 THEN
+			SELECT scoreToAdd INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
+		ELSE
+			scoreChange := 5;
+		END IF;
 
-        -- Mettre à jour le score de l'utilisateur
-        UPDATE SCORE SET score = score + scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
-    END LOOP;
+		UPDATE SCORE SET score = score + scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
+	END LOOP;
 END;
 /
 
+--------------------------------------------------------------------------------
 
---------------------------------------------------------------------------------------
-
--- i need a trigger that will create the login for users from their first name and last name and add record in score table
-CREATE OR REPLACE TRIGGER makeLogin
+CREATE OR REPLACE TRIGGER initUser
 FOR INSERT ON utilisateur
 COMPOUND TRIGGER
 
-    BEFORE EACH ROW IS
-    BEGIN
-        -- Génération du login pour chaque nouvel utilisateur
-        DECLARE
-            nomToUse VARCHAR2(7);
-        BEGIN
-            IF LENGTH(:NEW.nom) > 7 THEN
-                nomToUse := SUBSTR(:NEW.nom, 1, 7);
-            ELSE
-                nomToUse := :NEW.nom;
-            END IF;
-            :NEW.login := LOWER(SUBSTR(:NEW.prenom, 1, 1) || nomToUse);
-        END;
-    END BEFORE EACH ROW;
+	BEFORE EACH ROW IS
+	BEGIN
+		DECLARE
+			nomToUse VARCHAR2(7);
+		BEGIN
+			IF LENGTH(:NEW.nom) > 7 THEN
+				nomToUse := SUBSTR(:NEW.nom, 1, 7);
+			ELSE
+				nomToUse := :NEW.nom;
+			END IF;
+			:NEW.login := LOWER(SUBSTR(:NEW.prenom, 1, 1) || nomToUse);
+		END;
+	END BEFORE EACH ROW;
 
-    AFTER EACH ROW IS
-    BEGIN
-        -- Insertion dans la table SCORE après chaque insertion dans UTILISATEUR
-        INSERT INTO SCORE (idUtilisateur, score, niveau)
-        VALUES (:NEW.idUtilisateur, 0, 1);
-    END AFTER EACH ROW;
+	AFTER EACH ROW IS
+	BEGIN
+		INSERT INTO SCORE (idUtilisateur, score, niveau)
+		VALUES (:NEW.idUtilisateur, 0, 1);
+	END AFTER EACH ROW;
 
 END makeLogin;
 /
 
--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-CREATE OR REPLACE TRIGGER ClonePeriodicTasks
+CREATE OR REPLACE TRIGGER prodPeriodicite
 AFTER INSERT ON PERIODICITE
 FOR EACH ROW
 DECLARE
-    nextDate_v DATE;
+	nextDate_v DATE;
 BEGIN
-    -- Initialiser la prochaine date de la tâche périodique
-    nextDate_v := :NEW.dateDebut + :NEW.periode;
+	nextDate_v := :NEW.dateDebut + :NEW.periode;
 
-    -- Continuer à cloner la tâche jusqu'à la date de fin
-    WHILE nextDate_v <= :NEW.dateFin LOOP
-        -- Insertion d'une nouvelle tâche dans TACHENCOURS basée sur la tâche référencée
-        INSERT INTO TACHENCOURS (intitule, dateEcheance, lienExterne, categorie, status, idCreateur, idListe)
-        SELECT intitule, nextDate_v, lienExterne, categorie, status, idCreateur, idListe
-        FROM TACHENCOURS
-        WHERE idTache = :NEW.idTache;
+	WHILE nextDate_v <= :NEW.dateFin LOOP
+		INSERT INTO TACHENCOURS (intitule, dateEcheance, lienExterne, categorie, status, idCreateur, idListe)
+		SELECT intitule, nextDate_v, lienExterne, categorie, status, idCreateur, idListe
+		FROM TACHENCOURS
+		WHERE idTache = :NEW.idTache;
 
-        -- Calculer la prochaine date en fonction de la périodicité
-        nextDate_v := nextDate_v + :NEW.periode;
-    END LOOP;
+		nextDate_v := nextDate_v + :NEW.periode;
+	END LOOP;
 END;
 /
 
----------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-CREATE OR REPLACE TRIGGER UpdateLevel
+CREATE OR REPLACE TRIGGER updateLevel
 AFTER UPDATE OF score ON SCORE
 FOR EACH ROW
 DECLARE
-    nextLevel NUMBER;
+	nextLevel NUMBER;
 BEGIN
-    -- Calculer le seuil pour le niveau suivant
-    nextLevel := 100 * POWER(2, :NEW.niveau - 1);
+	nextLevel := 100 * POWER(2, :NEW.niveau - 1);
 
-    -- Vérifier si le score mis à jour atteint ou dépasse le seuil
-    IF :NEW.score >= nextLevelThreshold THEN
-        -- Mettre à jour le niveau
-        UPDATE SCORE SET niveau = niveau + 1 WHERE idUtilisateur = :NEW.idUtilisateur;
-    END IF;
+	IF :NEW.score >= nextLevelThreshold THEN
+		UPDATE SCORE SET niveau = niveau + 1 WHERE idUtilisateur = :NEW.idUtilisateur;
+	END IF;
 END;
 /
-
