c8c1bd5746d867a191a56639fe511eba4287139e
JBILOU GHAIT <gha.jbilou@gmail.com>
2024-01-07 12:49:57 +0100

    ajout de curseur dans les triggers

--- TodoListTriggers.sql
+++ TodoListTriggers.sql
@@ -2,27 +2,30 @@ CREATE OR REPLACE TRIGGER UpdateUserScore
 AFTER INSERT ON TACHEFINI
 FOR EACH ROW
 DECLARE
-    scoreChange NUMBER;
-    userHasProgrammeScore NUMBER;
+	scoreChange NUMBER;
+	userHasProgrammeScore NUMBER;
+	cursor c1 is
+		SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache;
+
 BEGIN
-    IF :NEW.status = 0 THEN
-        -- Pour chaque utilisateur associé à la tâche
-        FOR userRecord IN (SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache) LOOP
-            -- Vérifier si l'utilisateur a un programme score
-            SELECT COUNT(*) INTO userHasProgrammeScore FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
+	IF :NEW.status = 0 THEN
+		-- Pour chaque utilisateur associé à la tâche
+		FOR userRecord IN c1 LOOP
+			-- Vérifier si l'utilisateur a un programme score
+			SELECT COUNT(*) INTO userHasProgrammeScore FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
 
-            IF userHasProgrammeScore > 0 THEN
-                -- Récupérer le score à soustraire
-                SELECT scoreToSub INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
-            ELSE
-                -- Utiliser la valeur par défaut
-                scoreChange := 5;
-            END IF;
+			IF userHasProgrammeScore > 0 THEN
+				-- Récupérer le score à soustraire
+				SELECT scoreToSub INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
+			ELSE
+				-- Utiliser la valeur par défaut
+				scoreChange := 5;
+			END IF;
 
-            -- Mettre à jour le score
-            UPDATE SCORE SET score = score - scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
-        END LOOP;
-    END IF;
+			-- Mettre à jour le score
+			UPDATE SCORE SET score = score - scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
+		END LOOP;
+	END IF;
 END;
 /
 
@@ -32,18 +35,18 @@ CREATE OR REPLACE TRIGGER UpdateScoreAfterTaskCompletion
 AFTER UPDATE OF status ON TACHENCOURS
 FOR EACH ROW
 WHEN (NEW.status = 1)
+DECLARE
+	scoreChange NUMBER;
+	cursor c1 is
+		SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache;
 BEGIN
-    -- Pour chaque utilisateur associé à la tâche
-    FOR userRecord IN (SELECT idUtilisateur FROM TACHEUTILISATEUR WHERE idTache = :NEW.idTache) LOOP
-        -- Récupérer le score à ajouter
-        DECLARE
-            scoreChange NUMBER;
-        BEGIN
-            SELECT scoreToAdd INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
-            -- Mettre à jour le score de l'utilisateur
-            UPDATE SCORE SET score = score + scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
-        END;
-    END LOOP;
+	-- Pour chaque utilisateur associé à la tâche
+	FOR userRecord IN c1 LOOP
+	-- Récupérer le score à ajouter
+		SELECT scoreToAdd INTO scoreChange FROM PROGRAMMESCORE WHERE idCreateur = userRecord.idUtilisateur;
+		-- Mettre à jour le score de l'utilisateur
+		UPDATE SCORE SET score = score + scoreChange WHERE idUtilisateur = userRecord.idUtilisateur;
+	END LOOP;
 END;
 /
 
@@ -67,5 +70,3 @@ begin
 	end if;
 	:new.login := lower(substr(:new.prenom, 1, 1) || nomToUse);
 end;
-
-
